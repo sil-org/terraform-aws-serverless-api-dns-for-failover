9d7f1e8246f325c3e97e25c48bafeec8e69a31ad
Matt H <matt_henderson@sil.org>
2023-06-22 14:59:52 -0400

    Add usage example

---
+++ example/main.tf
@@ -0,0 +1,13 @@
+
+module "dns-for-failover" {
+  source = "github.com/silinternational/terraform-aws-serverless-api-dns-for-failover?ref=0.1.0"
+
+  app_name             = var.app_name
+  cloudflare_zone_name = var.cloudflare_zone_name
+  serverless_stage     = var.serverless_stage
+
+  providers = {
+    aws           = aws
+    aws.secondary = aws.secondary
+  }
+}
