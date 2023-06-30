758831a940efe7b5940c7ff626d309dcfccf8f14
briskt <3172830+briskt@users.noreply.github.com>
2023-06-30 09:57:41 +0800

    also output the domain names themselves (not just the summary text)

--- modules/fail-over-cname/outputs.tf
+++ modules/fail-over-cname/outputs.tf
@@ -1,5 +1,13 @@
+output "primary_region_domain_name" {
+  value = var.primary_region_domain_name
+}
+
 output "primary_region_summary" {
   value = local.primary_region_summary
+}
+
+output "secondary_region_domain_name" {
+  value = var.primary_region_domain_name
 }
 
 output "secondary_region_summary" {
