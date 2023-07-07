afbbc03bcea075d7060321fa487bb91be5bce635
briskt <3172830+briskt@users.noreply.github.com>
2023-07-07 11:21:11 +0800

    PR feedback: correct copy-paste error
    Co-authored-by: Baggerone <baggerone@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ output "primary_region_summary" {
 }
 
 output "secondary_region_domain_name" {
-  value = module.fail_over_cname.primary_region_domain_name
+  value = module.fail_over_cname.secondary_region_domain_name
 }
 
 output "secondary_region_summary" {
