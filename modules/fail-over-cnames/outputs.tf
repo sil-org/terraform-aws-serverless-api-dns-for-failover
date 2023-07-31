ec4c2ae83b01746d3e693512f724e05e24c21000
Matt H <matt_henderson@sil.org>
2023-07-31 17:00:32 -0400

    Add intermediate CNAMEs and clarify resource names accordingly

--- modules/fail-over-cnames/outputs.tf
+++ modules/fail-over-cnames/outputs.tf
@@ -1,13 +1,13 @@
-output "primary_region_domain_name" {
-  value = var.primary_region_domain_name
+output "primary_region_intermediate_domain" {
+  value = local.primary_region_intermediate_domain
 }
 
 output "primary_region_summary" {
   value = local.primary_region_summary
 }
 
-output "secondary_region_domain_name" {
-  value = var.secondary_region_domain_name
+output "secondary_region_intermediate_domain" {
+  value = local.secondary_region_intermediate_domain
 }
 
 output "secondary_region_summary" {
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 
 output "primary_region_domain_name" {
-  value = module.fail_over_cnames.primary_region_domain_name
+  value = module.fail_over_cnames.primary_region_intermediate_domain
 }
 
 output "primary_region_summary" {
@@ -8,7 +8,7 @@ output "primary_region_summary" {
 }
 
 output "secondary_region_domain_name" {
-  value = module.fail_over_cnames.secondary_region_domain_name
+  value = module.fail_over_cnames.secondary_region_intermediate_domain
 }
 
 output "secondary_region_summary" {
