6f9c38d7ae9bb4ba6ca01a106cec9ad8017ae3e3
Matt H <matt_henderson@sil.org>
2023-08-03 09:52:58 -0400

    Temporarily use feature branch to test a fix

--- modules/api-gateway-domains-and-certs/modules/certificate-and-domain/main.tf
+++ modules/api-gateway-domains-and-certs/modules/certificate-and-domain/main.tf
@@ -4,8 +4,9 @@ locals {
 }
 
 module "certificate" {
-  source  = "silinternational/acm-certificate/aws"
-  version = "0.2.1"
+  source = "github.com/silinternational/terraform-aws-acm-certificate?ref=feature%2Ffix-validation-when-not-creating-dns-record"
+  #  source  = "silinternational/acm-certificate/aws"
+  #  version = "0.2.1"
 
   certificate_domain_name = local.full_domain_name
   cloudflare_zone_name    = var.cloudflare_zone_name
