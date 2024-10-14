4cf0b468b030da9ca415df7367059f62c3624b37
briskt <3172830+briskt@users.noreply.github.com>
2024-10-14 18:11:16 +0800

    bring certificate resources into this repo to facilitate version changes

--- modules/api-gateway-domains-and-certs/modules/certificate-and-domain/main.tf
+++ modules/api-gateway-domains-and-certs/modules/certificate-and-domain/main.tf
@@ -3,21 +3,55 @@ locals {
   full_domain_name = "${var.certificate_subdomain}.${var.cloudflare_zone_name}"
 }
 
-module "certificate" {
-  source  = "silinternational/acm-certificate/aws"
-  version = "0.2.2"
-
-  certificate_domain_name = local.full_domain_name
-  cloudflare_zone_name    = var.cloudflare_zone_name
-  create_dns_validation   = var.create_dns_validation
-}
-
 module "domain" {
   source     = "github.com/silinternational/terraform-aws-api-gateway-custom-domain?ref=0.2.0"
-  depends_on = [module.certificate]
+  depends_on = [aws_acm_certificate_validation.this]
 
   api_name        = var.api_name
-  certificate_arn = module.certificate.certificate_arn
+  certificate_arn = aws_acm_certificate_validation.this.certificate_arn
   domain_name     = local.full_domain_name
   stage           = var.api_stage
+}
+
+
+data "cloudflare_zone" "this" {
+  name = var.cloudflare_zone_name
+}
+
+resource "aws_acm_certificate" "this" {
+  domain_name       = local.full_domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+moved {
+  from = module.certificate.aws_acm_certificate.this
+  to   = aws_acm_certificate.this
+}
+
+resource "cloudflare_record" "validation" {
+  count = var.create_dns_validation ? 1 : 0
+
+  name    = tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_name
+  value   = tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_value
+  type    = tolist(aws_acm_certificate.this.domain_validation_options)[0].resource_record_type
+  zone_id = data.cloudflare_zone.this.id
+  proxied = false
+}
+
+moved {
+  from = module.certificate.cloudflare_record.validation
+  to   = cloudflare_record.validation
+}
+
+resource "aws_acm_certificate_validation" "this" {
+  certificate_arn = aws_acm_certificate.this.arn
+}
+
+moved {
+  from = module.certificate.aws_acm_certificate_validation.this
+  to   = aws_acm_certificate_validation.this
 }
--- modules/api-gateway-domains-and-certs/modules/certificate-and-domain/versions.tf
+++ modules/api-gateway-domains-and-certs/modules/certificate-and-domain/versions.tf
@@ -7,7 +7,12 @@ terraform {
       source = "hashicorp/aws"
     }
     cloudflare = {
-      source = "cloudflare/cloudflare"
+      source  = "cloudflare/cloudflare"
+      version = ">= 4"
+
+      // 4.39.0 and later versions deprecated cloudflare_record.value
+      // constraining to earlier versions while waiting for version 5 to mature
+      version = ">= 2.0.0, < 4.39.0"
     }
   }
 }
