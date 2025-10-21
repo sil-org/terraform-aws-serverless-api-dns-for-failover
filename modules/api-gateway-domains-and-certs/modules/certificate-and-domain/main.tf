67b76ccb8099e1e496c6c52ee251a0697829a895
Jason Jackson <35783387+jason-jackson@users.noreply.github.com>
2025-10-21 07:55:09 -0400

    fix formatting issue
    Co-authored-by: briskt <3172830+briskt@users.noreply.github.com>

--- modules/api-gateway-domains-and-certs/modules/certificate-and-domain/main.tf
+++ modules/api-gateway-domains-and-certs/modules/certificate-and-domain/main.tf
@@ -4,8 +4,8 @@ locals {
 }
 
 module "domain" {
-  source  = "sil-org/api-gateway-custom-domain/aws"
-  version = "~> 0.2.0"
+  source     = "sil-org/api-gateway-custom-domain/aws"
+  version    = "~> 0.2.0"
   depends_on = [aws_acm_certificate_validation.this]
 
   api_name        = var.api_name
