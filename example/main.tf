1defffa9634c9e7ab38aac0a0da35c5e5b245476
Jason Jackson <35783387+jason-jackson@users.noreply.github.com>
2025-10-22 12:05:41 -0400

    Add disclaimer
    Co-authored-by: briskt <3172830+briskt@users.noreply.github.com>

--- example/main.tf
+++ example/main.tf
@@ -1,7 +1,7 @@
 
 module "dns_for_failover" {
   source  = "sil-org/serverless-api-dns-for-failover/aws"
-  version = "~> 0.6.0"
+  version = "~> 0.6.0" # This version number is only an example. Please use the latest version available.
 
   api_name             = var.api_name
   cloudflare_zone_name = var.cloudflare_zone_name
