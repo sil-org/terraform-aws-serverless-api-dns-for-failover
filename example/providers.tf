31d74e7864f8c47abeb7a8ce2a0d733fafdd9ba5
briskt <3172830+briskt@users.noreply.github.com>
2026-01-20 16:10:41 +0800

    Remove AWS access and secret keys from example

--- example/providers.tf
+++ example/providers.tf
@@ -1,14 +1,10 @@
 
 provider "aws" {
   region     = var.aws_region
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
 }
 
 provider "aws" {
   region     = var.aws_region_secondary
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
 
   alias = "secondary"
 }
